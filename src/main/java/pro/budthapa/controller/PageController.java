f6c280c1871b21db8f1cd40ee7c41edd5b6874df
Buddhi Bal Thapa <budthapa@gmail.com>
2017-05-03 12:56:12 +0400

    login page design

--- src/main/java/pro/budthapa/controller/PageController.java
+++ src/main/java/pro/budthapa/controller/PageController.java
@@ -7,9 +7,39 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class PageController {
 
 	private static final String INDEX_PAGE = "index";
+	private static final String RESUME_PAGE = "resume";
+	private static final String PROJECT_PAGE = "projects";
+	private static final String BLOG_PAGE = "blog";
+	private static final String CONTACT_PAGE = "contact";
+	private static final String LOGIN_PAGE = "login";
 
 	@GetMapping("/")
 	public String index(){
 		return INDEX_PAGE;
+	}
+	
+	@GetMapping("/resume")
+	public String resume(){
+		return RESUME_PAGE;
+	}
+	
+	@GetMapping("/project")
+	public String project(){
+		return PROJECT_PAGE;
+	}
+	
+	@GetMapping("/blog")
+	public String blog(){
+		return BLOG_PAGE;
+	}
+	
+	@GetMapping("/contact")
+	public String contact(){
+		return CONTACT_PAGE;
+	}
+	
+	@GetMapping("/login")
+	public String login(){
+		return LOGIN_PAGE;
 	}
 }
